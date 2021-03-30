2e0b1950a44c9bcc8165a980ddca7093ac3e76c8
fatma84 <fatma.dealzania@gmail.com>
2021-03-30 16:28:34 +0200

    facebook

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './components/home/home.module';
+import { LoginComponent } from './components/login/login.component';
+import { FacebookGuard } from './guards/facebook.guard';
+import { HomeComponent } from './components/home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+    canActivate : [FacebookGuard]
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import {MatCardModule} from '@angular/material/card';
+import { MaterialModule } from './shared/material.module';
+import { FormsModule } from '@angular/forms' ;
+import { ReactiveFormsModule} from '@angular/forms' 
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
